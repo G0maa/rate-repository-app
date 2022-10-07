dbf2498c8884ba10e6c041454f8e1402d0cf027e
G0maa <midomaxgomaa@gmail.com>
2022-10-07 18:31:55 +0200

    P10A - Ex 10.1 & 10.2 Done
    - Got the debugger working after some time

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
+if (__DEV__) {
+  console.log(window.__REACT_DEVTOOLS_PORT__);
+}
+
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  // requestIdleCallback(() => alert("works!"), { timeout: 1 });
+  console.log("test");
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! Test</Text>
+      <Text>Open up App.js to start working on your app! Tes2t</Text>
       <StatusBar style="auto" />
     </View>
   );
